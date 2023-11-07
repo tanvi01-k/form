8bd52eb5b896f05a319452b99ad292d4a16a6093
tanvi01-k <136151614+tanvi01-k@users.noreply.github.com>
2023-11-07 12:57:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+<script>
+// Get the login
+var modal = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == login) {
+    modal.style.display = "none";
+  }
+}
+</script>
